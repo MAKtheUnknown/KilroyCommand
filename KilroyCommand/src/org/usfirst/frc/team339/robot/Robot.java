b0570011cdd164f8b8a40d1ea80fd65f18f02487
Michael Andrzej Klaczynski <kilroyprogramming@gmail.com>
2016-05-26 18:35:46 -0400

    Nothing.

--- KilroyCommand/src/org/usfirst/frc/team339/robot/Robot.java
+++ KilroyCommand/src/org/usfirst/frc/team339/robot/Robot.java
@@ -18,82 +18,82 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class Robot extends IterativeRobot
 {
 
-public Autonomous autonomous;
-public DriverControl teleop;
-public static OI oi;
+	public Autonomous autonomous;
+	public DriverControl teleop;
+	public static OI oi;
 
 
-/**
- * This function is run when the robot is first started up and should be
- * used for any initialization code.
- */
-public void robotInit ()
-{
-    oi = new OI();
-}
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit ()
+	{
+		oi = new OI();
+	}
 
-/**
- * This function is called once each time the robot enters Disabled mode.
- * You can use it to reset any subsystem information you want to clear when
- * the robot is disabled.
- */
-public void disabledInit ()
-{
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	public void disabledInit ()
+	{
 
-}
+	}
 
-public void disabledPeriodic ()
-{
-    //Scheduler.getInstance().run();
-}
+	public void disabledPeriodic ()
+	{
+		//Scheduler.getInstance().run();
+	}
 
-/**
- * Start autonomous.
- * Passes in the value of the six-position switch for a starting position.
- */
-public void autonomousInit ()
-{
-    int lane = Hardware.startingPositionDial.getPosition() + 1;
-    this.autonomous = new Autonomous(lane);
-    this.autonomous.start();
-}
+	/**
+	 * Start autonomous.
+	 * Passes in the value of the six-position switch for a starting position.
+	 */
+	public void autonomousInit ()
+	{
+		int lane = Hardware.startingPositionDial.getPosition() + 1;
+		this.autonomous = new Autonomous(lane);
+		this.autonomous.start();
+	}
 
-/**
- * This function is called periodically during autonomous
- */
-public void autonomousPeriodic ()
-{
-    Scheduler.getInstance().run();
-}
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic ()
+	{
+		Scheduler.getInstance().run();
+	}
 
-public void teleopInit ()
-{
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to 
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomous != null)
-        {
-        autonomous.cancel();
-        }
-    teleop = new DriverControl();
-    teleop.start();
+	public void teleopInit ()
+	{
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to 
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomous != null)
+		{
+			autonomous.cancel();
+		}
+		teleop = new DriverControl();
+		teleop.start();
 
-}
+	}
 
-/**
- * This function is called periodically during operator control
- */
-public void teleopPeriodic ()
-{
-    Scheduler.getInstance().run();
-}
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic ()
+	{
+		Scheduler.getInstance().run();
+	}
 
-/**
- * This function is called periodically during test mode
- */
-public void testPeriodic ()
-{
-    LiveWindow.run();
-}
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic ()
+	{
+		LiveWindow.run();
+	}
 }
